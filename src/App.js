90a4c728aba0b36f39a07eaead28fa1936a73eb1
Raghav <43498142+Raghav10026@users.noreply.github.com>
2020-12-19 02:58:44 +0530

    Update: database addition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
 import React from "react";
 import "./styles.css";
+const top10Database={
+  Youtube:[
+    { name: "Baby Shark Dance", views: "9/10" },
+    { name: "Despacito", views: "8.8/10" },
+    { name: "Shape of You", views: "8.7/10" },
+    { name: "See You Again", views: "8.6/10" },
+    { name: "Masha and the Bear", views: "8/10" },
+    { name: "Johny Johny Yes Papa", views: "7.8/10" },
+    { name: "Uptown Funk", views: "7.5/10" }
+    { name: "Gangnam Style", views: "7.5/10" }
+    { name: "Learning Colors – Colorful Eggs on a Farm", views: "7.5/10" }
+    { name: "Sorry", views: "7.5/10" }
+  ],
+  Spoify Streams:[
+    { name: "Baby Shark Dance", views: "9/10" },
+    { name: "Despacito", views: "8.8/10" },
+    { name: "Shape of You", views: "8.7/10" },
+    { name: "See You Again", views: "8.6/10" },
+    { name: "Masha and the Bear", views: "8/10" },
+    { name: "Johny Johny Yes Papa", views: "7.8/10" },
+    { name: "Uptown Funk", views: "7.5/10" }
+    { name: "Gangnam Style", views: "7.5/10" }
+    { name: "Learning Colors – Colorful Eggs on a Farm", views: "7.5/10" }
+    { name: "Sorry", views: "7.5/10" }
+  ],
+  Youtube:[
+    { name: "Baby Shark Dance", views: "9/10" },
+    { name: "Despacito", views: "8.8/10" },
+    { name: "Shape of You", views: "8.7/10" },
+    { name: "See You Again", views: "8.6/10" },
+    { name: "Masha and the Bear", views: "8/10" },
+    { name: "Johny Johny Yes Papa", views: "7.8/10" },
+    { name: "Uptown Funk", views: "7.5/10" }
+    { name: "Gangnam Style", views: "7.5/10" }
+    { name: "Learning Colors – Colorful Eggs on a Farm", views: "7.5/10" }
+    { name: "Sorry", views: "7.5/10" }
+  ],
+  Youtube:[
+    { name: "Baby Shark Dance", views: "9/10" },
+    { name: "Despacito", views: "8.8/10" },
+    { name: "Shape of You", views: "8.7/10" },
+    { name: "See You Again", views: "8.6/10" },
+    { name: "Masha and the Bear", views: "8/10" },
+    { name: "Johny Johny Yes Papa", views: "7.8/10" },
+    { name: "Uptown Funk", views: "7.5/10" }
+    { name: "Gangnam Style", views: "7.5/10" }
+    { name: "Learning Colors – Colorful Eggs on a Farm", views: "7.5/10" }
+    { name: "Sorry", views: "7.5/10" }
+  ],
+}
 
 export default function App() {
   return (
