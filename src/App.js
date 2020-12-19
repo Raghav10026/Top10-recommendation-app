7dd1c8a87b633d9358a81d7b5736ac13f8dd7b04
Raghav <43498142+Raghav10026@users.noreply.github.com>
2020-12-19 20:59:28 +0530

    Update: model done

--- src/App.js
+++ src/App.js
@@ -1,52 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-const youtubeGenres={
-  Music:[
+const youtubeGenres = {
+  Music: [
     { name: "Despacito", views: "7 billion" },
     { name: "Shape of You", views: "5 billion" },
     { name: "See You Again", views: "4.7 billion" },
     { name: "Uptown Funk", views: "3.9 billion" },
-    { name: "Gangnam Style", views: "3.8 billion" },
+    { name: "Gangnam Style", views: "3.8 billion" }
   ],
 
-  Gaming:[
+  Gaming: [
     { name: "Minecraft", views: "102 Billion" },
     { name: "Fortnite", views: "60 Billion" },
     { name: "Grand Theft Auto V", views: "40 Billion" },
     { name: "Garena Free Fire", views: "32 Billion" },
-    { name: "Roblox", views: "30 Billion" },
+    { name: "Roblox", views: "30 Billion" }
   ],
 
-  News:[
+  News: [
     { name: "Aaj Tak", views: "13 Billion" },
     { name: "ABP News", views: "7 Billion" },
     { name: "ABP News Hindi", views: "5.5 Billion" },
     { name: "DLS News", views: "1.5 Billion" },
-    { name: " Raffy Tulfo in Action", views: "81 Million" },
+    { name: " Raffy Tulfo in Action", views: "81 Million" }
   ],
-
-  Films:[
+  Films: [
     { name: "Avengers Endgame", views: "289 Million" },
     { name: "Avenger Infinity War", views: "230 Million" },
     { name: "Lion King", views: "224 Million" },
     { name: "IT", views: "197 Million" },
-    { name: "Mulan", views: "175 Million" },
+    { name: "Mulan", views: "175 Million" }
   ],
-
-  Fashion & Beauty:[
+  Fashion: [
     { name: "Kylie Jenner: A Day in the Life", views: "51 Million" },
-    { name: "Kylie Skin Review with Shane Dawson, Jeffree Star", views: "32 Million" },
+    {
+      name: "Kylie Skin Review with Shane Dawson Jeffree Star",
+      views: "32 Million"
+    },
     { name: "Foundation Routine For Acne by Cassandra", views: "29 Million" },
-    { name: "The Beautiful World of Jeffree Star by Shane", views: "28 Million" },
-    { name: "Makeup Tutorial en Español by James Charles", views: "27 Million" },
-  ],
-}
+    {
+      name: "The Beautiful World of Jeffree Star by Shane",
+      views: "28 Million"
+    },
+    { name: "Makeup Tutorial en Español by James Charles", views: "27 Million" }
+  ]
+};
+var youtube = Object.keys(youtubeGenres);
 
 export default function App() {
+  const [userGenre, setgenre] = useState("Music");
+
+  function OnClickHandler(item) {
+    setgenre(item);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2 className="head">
+        <b>Top 5 RECOMMENDATION FOR YOU</b>
+      </h2>
+      <hr />
+      <div className="one">
+        <h4 style={{ color: "#164A41" }}>
+          <b> Check Top 5 Youtube videos in each Genre.</b>
+          <em>Select a genre to get started </em>
+        </h4>
+        <div>
+          {youtube.map((item) => (
+            <button className="btn" onClick={() => OnClickHandler(item)}>
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="two">
+        <div className="body">
+          <ul style={{ paddingInlineStart: "0" }}>
+            {youtubeGenres[userGenre].map((youtube) => (
+              <li className="li">
+                <div className="div1" key={youtube.name}>
+                  <b> {youtube.name}</b>
+                </div>
+                <div className="div2" key={youtube.views}>
+                  <strong> views: {youtube.views} </strong>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
